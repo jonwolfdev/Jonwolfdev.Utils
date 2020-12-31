a5f285e75d71ac51f48b68812a50b830fe2b8b00
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2020-12-31 09:27:08 -0600

    Add project files.

---
+++ Jonwolfdev.Utils.Common/Dates/Date.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jonwolfdev.Utils.Common.Dates
+{
+    public class Date
+    {
+        DateTimeOffset _date;
+        public DateTimeOffset DateTimeOffset { get => _date; set { _date = value; } }
+        public DateTimeOffset OnlyUtcDate
+        {
+            get => _date.UtcDateTime.Date;
+        }
+        public DateTimeOffset OnlyLocalDate
+        {
+            get => _date.LocalDateTime.Date;
+        }
+        public Date()
+        {
+            // For serialization
+        }
+        public Date(DateTimeOffset dt)
+        {
+            _date = dt.UtcDateTime.Date;
+        }
+    }
+}
---
+++ Jonwolfdev.Utils.Common/Validation/ObjectValidator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Jonwolfdev.Utils.Common.Validation
+{
+    public static class ObjectValidator
+    {
+        public static void Validate(object objx, string paramName)
+        {
+            if (objx == null)
+                throw new ArgumentNullException(nameof(objx));
+
+            if (objx is System.Collections.IEnumerable objList)
+            {
+                foreach (var objItem in objList)
+                {
+                    if (objItem == null)
+                    {
+                        throw new Exception($"{nameof(ObjectValidator)} Item in list is null. List type: {objx.GetType().Name}");
+                    }
+                    Validate(objItem, objItem.GetType().Name);
+                }
+            }
+
+            var list = new List<ValidationResult>();
+            bool isValid = Validator.TryValidateObject(objx, new ValidationContext(objx), list, true);
+
+            if (!isValid)
+            {
+                string message = string.Join("\r\n", list);
+                throw new ArgumentException(message, paramName);
+            }
+
+            //`objx` must be object for this to work
+            var propertyList = objx.GetType()
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var objects = propertyList.Where(x => x.IsDefined(typeof(ValidateComplexObjectAttribute))).ToList();
+            var lists = propertyList.Where(x => x.IsDefined(typeof(ValidateComplexListAttribute))).ToList();
+
+            foreach (var prop in objects)
+            {
+                var propVal = prop.GetValue(objx);
+                if (propVal != null)
+                {
+                    Validate(propVal, prop.Name);
+                }
+            }
+
+            foreach (var prop in lists)
+            {
+                if (prop.GetValue(objx) is System.Collections.IEnumerable propVal)
+                {
+                    foreach (var listVal in propVal)
+                    {
+                        Validate(listVal, prop.Name);
+                    }
+                }
+            }
+        }
+    }
+}
