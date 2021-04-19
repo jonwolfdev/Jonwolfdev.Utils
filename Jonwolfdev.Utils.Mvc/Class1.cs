27057893cae8b97424cae9ec79f0130e8777f417
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2021-04-19 09:40:55 -0500

    Test lib mvc

---
+++ Jonwolfdev.Utils.Mvc/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jonwolfdev.Utils.Mvc
+{
+    public class Class1
+    {
+        public string Ok(Microsoft.AspNetCore.Http.HttpContext c)
+        {
+            return c.Connection.RemoteIpAddress.ToString();
+        }
+    }
+}
