19016df501c528ca6d905f5f28cb66ab55f30580
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2022-01-06 16:59:11 -0600

    Fix nuget repo url Add PropertyLinkAttribute Bump version

---
+++ Jonwolfdev.Utils6.Tests/Misc/PropertyLinkAttributeTests.cs
@@ -0,0 +1,27 @@
+﻿using Jonwolfdev.Utils6.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Jonwolfdev.Utils6.Tests.Misc
+{
+    public class PropertyLinkAttributeTests
+    {
+        [Fact]
+        public void Should_Create_Class()
+        {
+            // act & assert
+            new PropertyLinkAttribute(1);
+        }
+
+        [Fact]
+        public void Should_Throw_ArgumentException_Invalid_Id()
+        {
+            // act & assert
+            Assert.Throws<ArgumentException>(() => new PropertyLinkAttribute(-1));
+        }
+    }
+}
---
+++ Jonwolfdev.Utils6/Misc/PropertyLinkAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jonwolfdev.Utils6.Misc
+{
+    /// <summary>
+    /// This is a helper attribute to link the same property in different models.
+    /// For example, Name can be inside entity, read model (or dto), etc.
+    /// If you add this attribute (PropertyLink("ImageName")) to both properties in different classes,
+    /// it will be easier to identify.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, Inherited = false)]
+    public class PropertyLinkAttribute : Attribute
+    {
+        public PropertyLinkAttribute(int propId)
+        {
+            if (propId < 0)
+                throw new ArgumentException("Must be greater than -1", nameof(propId));
+        }
+    }
+}
