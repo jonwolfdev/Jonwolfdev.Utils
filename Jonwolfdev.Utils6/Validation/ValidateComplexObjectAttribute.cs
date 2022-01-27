06d0fcb230c007a2344f7a2934f715c320e83f9d
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2022-01-27 17:33:47 -0600

    Add validation

---
+++ Jonwolfdev.Utils6/Validation/ValidateComplexObjectAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jonwolfdev.Utils.Common.Validation
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ValidateComplexObjectAttribute : Attribute
+    {
+    }
+}
